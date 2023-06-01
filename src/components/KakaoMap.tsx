47928439945935eb8c81801627fd8d9316c80e43
hyunsik <hyunsig96@gmail.com>
2023-06-01 20:43:47 +0900

    add: Map 내 필요 component들 백 서버 연결 가정 기능 구현

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,8 +1,10 @@
+import { useState, useEffect, useRef, KeyboardEventHandler } from "react";
 import { Map, MapMarker, useInjectKakaoMapApi } from "react-kakao-maps-sdk";
 import style from "./KakaoMap.module.css";
 import { Outlet } from 'react-router-dom';
 import useGeoLocation from "../hooks/useGeolocation";
-import { useState, useEffect } from "react";
+import ModalPortal from "./mapModal/ModalPortal";
+import { CSSTransition } from "react-transition-group";
 
 interface Store {
   title: string;
@@ -13,53 +15,79 @@ interface Store {
 }
 
 const KakaoMap = () => {
-  const location = useGeoLocation();
+  const location = useGeoLocation();  // 외부 훅 컴포넌트에서 받아온 위치정보
+  const [showModal, setShowModal] = useState(false);
   const [center, setCenter] = useState({ lat: 33.450701, lng: 126.570677 })
+  const [isFlyer, setIsFlyer] = useState(true);
+  const [flyerUrl, setFlyerUrl] = useState('/public/image/greenFlyer.svg');
+  const [couponUrl, setCouponUrl] = useState('/public/image/whiteCoupon.svg');
   const { loading, error } = useInjectKakaoMapApi({
     appkey: import.meta.env.VITE_KAKAO_API_KEY,
     libraries: ['services'],
   });
-
+  const search = useRef<HTMLInputElement>(null);
   // 초기 좌표 기준으로 설정해두기
   const [stores, setStores] = useState<Store[]>([]);
-
+  
   // 위치정보 불러와지는 것 감시한 뒤 이동시키기
   useEffect(()=>{
     setCenter({ lat: location.loaded ? location.coordinates!.lat : 33.450701, lng: location.loaded ? location.coordinates!.lng : 126.570677 });
   }, [location.loaded]);
 
-  // 지도의 중심좌표에 따라 서버에 주변 점포 요청해서 set해주기
   useEffect(()=>{
+    // TODO: 지도의 중심좌표에 따라 서버에 주변 점포 요청해서 set해주기
     setStores(
       [{
         title: "카카오",
         latlng: { lat: 33.450705, lng: 126.570677 },
-      },
-      {
-        title: "생태연못",
-        latlng: { lat: 33.450936, lng: 126.569477 },
-      },
-      {
-        title: "텃밭",
-        latlng: { lat: 33.450879, lng: 126.56994 },
-      },
-      {
-        title: "근린공원",
-        latlng: { lat: 33.451393, lng: 126.570738 },
-      },
+      }
     ])
-    console.log(center);
-  }, [center]);
+    console.log(center, isFlyer);
+  }, [center, isFlyer, showModal]);
   
   if (loading || error) return <></>;
 
-  /*
-    map을 가장 뒤에 꽉채워놔서 앞으로 올 것들은 아래처럼 이 위에 올라와야 한다.
-    .overlay-div {
-    position: relative;
-    z-index: 3;
+  const onClickHandler = () => {
+    setShowModal(false);
+  };
+
+  const changeToFlyer = () => {
+    if (!isFlyer){
+      setIsFlyer(true);
+      setFlyerUrl('/public/image/greenFlyer.svg');
+      setCouponUrl('/public/image/whiteCoupon.svg');
     }
-  */
+  };
+
+  const changeToCoupon = () => {
+    if (isFlyer){
+      setIsFlyer(false);
+      setFlyerUrl('/public/image/whiteFlyer.svg');
+      setCouponUrl('/public/image/greenCoupon.svg');
+    }
+  };
+
+  const getLocation = () => {
+    if(location.loaded){
+      setCenter({lat: location.coordinates!.lat, lng: location.coordinates!.lng});
+    }else{
+      window.location.reload();
+    }
+  };
+
+  const enterSearch: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if(event.key === 'Enter' && search.current!.value !== ''){
+      console.log(search.current!.value);
+      // TODO: search.current!.value 연관검색어 리스트 서버에 받아와서 아래 set 해주기
+      setStores(
+        [{
+          title: "카카오",
+          latlng: { lat: 33.450705, lng: 126.570677 },
+        }
+      ])
+      setShowModal(true);
+    }
+  };
 
   return (
     <>
@@ -79,7 +107,7 @@ const KakaoMap = () => {
           key={`${position.title}-${position.latlng}`}
           position={position.latlng} // 마커를 표시할 위치
           image={{
-            src: "/public/mapMarker.png", // 마커이미지의 주소입니다
+            src: "/public/image/mapMarker.png", // 마커이미지의 주소입니다
             size: {
               width: 27.73,
               height: 33,
@@ -90,9 +118,29 @@ const KakaoMap = () => {
       ))}
     </Map>
     </div>
+    <div className={style.divForShadow}/>
+    <input className={style.searchBox} placeholder="가게 이름을 검색하세요" ref={search} onKeyUpCapture={enterSearch}/>
+    <img src="/public/image/searchIcon.svg" className={style.searchIcon}/>
+    <img src={flyerUrl} className={style.flyer} onClick={() => changeToFlyer()} />
+    <img src={couponUrl} className={style.coupon} onClick={() => changeToCoupon()} />
+    <img src="/public/image/findMyGeo.svg" className={style.findMyGeo} onClick={() => getLocation()} />
+    <img src="/public/image/slideUpBtn.svg" className={style.slideUpBtn} onClick={() => setShowModal(true)} />
+    <ModalPortal show={showModal} onClick={onClickHandler}>
+        <CSSTransition
+          mountOnEnter
+          unmountOnExit
+          in={showModal}
+          timeout={{ enter: 300, exit: 300 }}
+          classNames="modal"
+        >
+          <div className="modal">
+            <h2>지점들 정보 리스트</h2>
+          </div>
+        </CSSTransition>
+    </ModalPortal>
     <Outlet/>
     </>
   );
 }
-  
-export default KakaoMap
+
+export default KakaoMap;
