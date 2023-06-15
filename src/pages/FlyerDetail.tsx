5fc1c3d2fe1c35d091bb60b9c8f17a0c6ee635a1
juhongahn <juhong.ahn.dev@gmail.com>
2023-06-15 23:53:19 +0900

    fix: qr 로깅 기능 수정

--- src/pages/FlyerDetail.tsx
+++ src/pages/FlyerDetail.tsx
@@ -1,56 +1,59 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getAuthToken } from "../util/auth";
+// import React, { useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import { getAuthToken } from "../util/auth";
 
-import FullFyler from "./flyer/FullFlyer";
+// import FullFyler from "./flyer/FullFlyer";
 
-const FlyerDetailPage: React.FC = () => {
-  const { flyerId, qrId, storeIdx } = useParams();
-  console.log(flyerId);
-  console.log(qrId);
-  console.log(storeIdx);
-  const token = getAuthToken();
-  //const client = getDupClient();
+// const FlyerDetailPage: React.FC = () => {
+//   const { flyerId, qrId, storeIdx } = useParams();
+//   console.log(flyerId);
+//   console.log(qrId);
+//   console.log(storeIdx);
+//   const token = getAuthToken();
+//   //const client = getDupClient();
 
-  useEffect(() => {
-    const scanCode = async () => {
-      const response = await fetch("https://qrecode-back.shop/qr/logging", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token ? token : "none-member"}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          storeIdx: storeIdx,
-          flyerIdx: flyerId,
-          qrIdx: qrId,
-        }),
-      });
+//   useEffect(() => {
+//     const scanCode = async () => {
+//       console.log(flyerId);
+//       console.log(qrId);
+//       console.log(storeIdx);
+//       const response = await fetch("https://qrecode-back.shop/qr/logging", {
+//         method: "POST",
+//         headers: {
+//           Authorization: `Bearer ${token ? token : "none-member"}`,
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           storeIdx: storeIdx,
+//           flyerIdx: flyerId,
+//           qrIdx: qrId,
+//         }),
+//       });
 
-      if (!response.ok) {
-        throw new Error("서버에서 문제가 발생했습니다.");
-      }
-    };
-    scanCode();
-    // 이미 스캔한 대상이 아니라면.
-    // if (storeIdx && flyerId && qrId && !client) {
-    //   setDupClient(storeIdx, flyerId, qrId);
-    //   scanCode();
-    // }
-  }, []);
+//       if (!response.ok) {
+//         throw new Error("서버에서 문제가 발생했습니다.");
+//       }
+//     };
+//     scanCode();
+//     // 이미 스캔한 대상이 아니라면.
+//     // if (storeIdx && flyerId && qrId && !client) {
+//     //   setDupClient(storeIdx, flyerId, qrId);
+//     //   scanCode();
+//     // }
+//   }, []);
 
-  // TODO: 전단지 렌더링.
-  return <FullFyler />;
-};
-
-// const setDupClient = (storeIdx: string, flyerId: string, qrId: string) => {
-//   localStorage.setItem(
-//     "client",
-//     JSON.stringify({ storeIdx: storeIdx, flyerId: flyerId, qrId: qrId })
-//   );
-// };
-// const getDupClient = () => {
-//   return localStorage.getItem("client");
+//   // TODO: 전단지 렌더링.
+//   return <>hello</>;
 // };
 
-export default FlyerDetailPage;
+// // const setDupClient = (storeIdx: string, flyerId: string, qrId: string) => {
+// //   localStorage.setItem(
+// //     "client",
+// //     JSON.stringify({ storeIdx: storeIdx, flyerId: flyerId, qrId: qrId })
+// //   );
+// // };
+// // const getDupClient = () => {
+// //   return localStorage.getItem("client");
+// // };
+
+// export default FlyerDetailPage;
