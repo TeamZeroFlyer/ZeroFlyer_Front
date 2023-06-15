7dbac46715ec636e32521485c634dbb0abc347c1
hyunsik <hyunsig96@gmail.com>
2023-06-15 18:24:29 +0900

    feat: 링크유입 전단지저장 분기

--- src/pages/flyer/FlyerDetail.tsx
+++ src/pages/flyer/FlyerDetail.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useOutletContext, useParams } from "react-router-dom";
 import { getAuthToken } from "../../util/auth";
 import style from "./FlyerDetail.module.css";
-
+type ChildProps = {
+  status: number
+}
 // localhost:5173/flyer/:flyerId/qr/:qrId/
 const FlyerDetailPage: React.FC = () => {
   const { flyerId, qrId } = useParams();
   const token = getAuthToken();
   const client = getDupClient();
+  const [imgUrl, setImgUrl] = React.useState<string>("");
+  const {status} = useOutletContext<ChildProps>();
 
   useEffect(() => {
     const scanCode = async () => {
@@ -28,7 +32,7 @@ const FlyerDetailPage: React.FC = () => {
     }
 
     //Todo: 서버url 확인
-    fetch("https://qrecode-back.shop/store/info" + flyerId, {
+    fetch("https://qrecode-back.shop/user/flyer/show?idx=" + flyerId, {
       method: "GET",
       headers: {
           "Content-Type": "application/json"
@@ -38,20 +42,52 @@ const FlyerDetailPage: React.FC = () => {
           return response.json()
       })
       .then(data => {
-        data.data
+        setImgUrl(data.data);
     });
 
   }, []);
+
+  const save = () => {
+    if(status === 0){
+      sessionStorage.setItem("save", flyerId!);
+      alert("로그인이 필요합니다.");
+      window.location.href = "/login";
+    }else{
+      fetch("https://qrecode-back.shop/user/saveflyer?idx=" + flyerId, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+            },
+        })
+        .then(response => {
+          if(!response.ok){
+            alert('이미 저장되어있는 전단지입니다.');
+            window.location.href = "/";
+            return;
+          }
+            return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          if(data.data === 'success'){
+            alert('저장되었습니다.');
+            window.location.href = "/";
+          }
+      });
+
+    }
+  }
 
   return (
     <>
     <div className={style.container}>
         <div className={style.imgBox}>
-            <img className={style.img} src={''} alt='https://raw.githubusercontent.com/TeamZeroFlyer/ZeroFlyer_Front/9be89183664a4898914b84dece371161ba044478/public/icons/plus.svg'/>
+            <img className={style.img} src={imgUrl} alt='https://raw.githubusercontent.com/TeamZeroFlyer/ZeroFlyer_Front/9be89183664a4898914b84dece371161ba044478/public/icons/plus.svg'/>
         </div>
         <div className={style.twoBtn}>
-            <div className={style.btn} onClick={()=>{}}><span className={style.green}>전단지</span>삭제</div>
-            <div className={style.btn2}>전단지 <span className={style.green}>쿠폰</span>을<br></br> 저장해요!</div>
+            {status !== 0 && <Link to='/'><div className={style.btn} onClick={()=>{}}><span className={style.green}>홈</span>으로 이동</div></Link>}
+            <div className={style.btn2} onClick={()=>save()}>전단지 <span className={style.green}>쿠폰</span>을<br></br> 저장해요!</div>
         </div>
     </div>
     </>
--- src/pages/flyer/FullFlyer.tsx
+++ src/pages/flyer/FullFlyer.tsx
@@ -20,7 +20,7 @@ const FullFyler = () => {
                 return response.json()
             })
             .then(data => {
-                if (data.status === 200){
+                if (data.data === 'success'){
                     alert("삭제되었습니다.");
                     window.location.href = "/flyer";
                 }else{
@@ -32,12 +32,13 @@ const FullFyler = () => {
     return (
         <>
         <div className={style.container}>
+        <div className={style.topBtn}><Link to="/flyer"><img className={style.img} src='https://raw.githubusercontent.com/TeamZeroFlyer/ZeroFlyer_Front/9be89183664a4898914b84dece371161ba044478/public/icons/backBtn.svg'/></Link></div>
             <div className={style.imgBox}>
                 <img className={style.img} src={receivedString ? receivedString : 'https://raw.githubusercontent.com/TeamZeroFlyer/ZeroFlyer_Front/9be89183664a4898914b84dece371161ba044478/public/icons/plus.svg'} alt='https://raw.githubusercontent.com/TeamZeroFlyer/ZeroFlyer_Front/9be89183664a4898914b84dece371161ba044478/public/icons/plus.svg'/>
             </div>
             <div className={style.twoBtn}>
                 <div className={style.btn} onClick={()=>deleteFlyer()}><span className={style.green}>전단지</span>삭제</div>
-                <div className={style.btn2}>전단지 <span className={style.green}>쿠폰</span>을<br></br> 저장해요!</div>
+                {/* <div className={style.btn2}>전단지 <span className={style.green}>쿠폰</span>을<br></br> 저장해요!</div> */}
             </div>
         </div>
         </>
